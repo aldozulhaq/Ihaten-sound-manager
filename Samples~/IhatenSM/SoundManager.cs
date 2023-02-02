ff873d1059bdebfa9c8d92079ff16584a0652995
Aldo Zulhaq <44134685+aldozulhaq@users.noreply.github.com>
2023-02-03 03:37:57 +0700

    Add files via upload

---
+++ Samples~/IhatenSM/SoundManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace Ihaten.SoundManager
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager Instance;
+
+        public AudioSource _BGMSource, _SFXSource;
+        public SoundDatabase _Database;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void PlaySound(AudioClip clip)
+        {
+            _SFXSource.pitch = 1;
+            _SFXSource.PlayOneShot(clip);
+        }
+
+        public void PlayMusic(AudioClip clip)
+        {
+            _BGMSource.clip = clip;
+            _BGMSource.Play();
+        }
+
+        public void PlaySoundRandomPitch(AudioClip clip)
+        {
+            _SFXSource.pitch = Random.Range(0.5f, 1.8f);
+            _SFXSource.PlayOneShot(clip);
+        }
+
+        public void StopMusic()
+        {
+            _BGMSource.Stop();
+        }
+
+        public void PauseMusic()
+        {
+            _BGMSource.Pause();
+        }
+
+        public void ResumeMusic()
+        {
+            _BGMSource.UnPause();
+        }
+    }
+}
